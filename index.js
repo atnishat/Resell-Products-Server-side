e2172b0ce31d4f9ac6ca3a99adef14c34dadecf2
atnishat <nishat.mu47@gmail.com>
2022-11-27 15:35:58 +0600

    admin checked

--- index.js
+++ index.js
@@ -115,7 +115,12 @@ try{
         res.status(403).send({ accessToken: '' })
     });
 
-    
+       app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
 
     app.post('/users', async (req, res) => {
         const user = req.body;
